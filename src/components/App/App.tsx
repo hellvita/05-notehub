4659d171b04f7f84bbe3be1359c8d4304a6dfaa5
hellvita <seredaolga03@gmail.com>
2025-12-02 20:29:30 +0200

    add the fetchNotes function

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,16 @@
+import { fetchNotes } from "../../services/noteService";
+
 export default function App() {
+  const handle = async () => {
+    try {
+      const { notes, totalPages } = await fetchNotes({});
+      console.log("totalPages: ", totalPages);
+      console.log("notes: ", notes);
+      console.log("note-1: ", notes[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  handle();
   return <></>;
 }
