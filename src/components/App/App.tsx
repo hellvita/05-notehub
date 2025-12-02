44da1feee8adc7ac1be79f69e4c7c428e9e9ee3c
hellvita <seredaolga03@gmail.com>
2025-12-02 20:50:56 +0200

    add the header element to the App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,5 @@
-// import { fetchNotes } from "../../services/noteService";
-// import { createNote } from "../../services/noteService";
-
-import { deleteNote } from "../../services/noteService";
+// import { fetchNotes, createNote, deleteNote  } from "../../services/noteService";
+import css from "./App.module.css";
 
 export default function App() {
   const handle = async () => {
@@ -10,19 +8,26 @@ export default function App() {
       // console.log("totalPages: ", totalPages);
       // console.log("notes: ", notes);
       // console.log("note-1: ", notes[0]);
-
       // const createdNote = await createNote({
       //   title: "my custom note",
       //   tag: "Personal",
       // });
       // console.log("createdNote: ", createdNote);
-
-      const deletedNote = await deleteNote("cmiox7cwf1dzlyy8u5nel2tcn");
-      console.log("deletedNote: ", deletedNote);
+      // const deletedNote = await deleteNote("cmiox7cwf1dzlyy8u5nel2tcn");
+      // console.log("deletedNote: ", deletedNote);
     } catch (error) {
       console.log(error);
     }
   };
   handle();
-  return <></>;
+
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        {/* Компонент SearchBox */}
+        {/* Пагінація */}
+        {/* Кнопка створення нотатки */}
+      </header>
+    </div>
+  );
 }
